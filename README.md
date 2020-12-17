01f10080367f4495236940a8fdf796bb909f32b5
Johannes Wipper <joe.wipper@gmx.de>
2020-12-17 16:11:34 +0100

    tidy up; add start and stop

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # CampusNet to Calendar
 This package synchronises your CampusNet dates to your Apple Calendar for the current and the next month. This package mainly exists because the CampusNet App has a bug where users get logged out regularly and date changes won't update. As this bug probably will not be fixed anytime soon, this package will act as an interim solution.
 
-> Note: If you got here by browsing this package is probably not for you. Unless you also want to synchronise dates from a CampusNet to your Apple Calendar, in which case you want to change the URL in cn-scraper.js
+> Note: If you got here by browsing this package is probably not for you. Unless you also want to synchronise dates from a CampusNet to your Apple Calendar, in which case you want to change the URL in lib/cn-scraper.js
+
+- [How it works](#how-it-works)
+- [Installation](#installation)
+  - [Where to install](#where-to-install)
+  - [How to install](#how-to-install)
+- [Usage](#usage)
+  - [Setup](#setup)
+  - [Starting](#starting)
+  - [Stopping](#stopping)
+- [Contributing](#contributing)
 
 ### How it works
 When you run the script on your computer, it will check twice every day if there were any changes to your CampusNet dates and adjusts them in your cloud calendar. Your calendar app will then fetch the changes and show the correct dates on your phone. Here, I made a figure:  
 
-![Figure 1](figure1.svg)
+![Figure 1](img/figure1.svg)
 
 ## Installation
 ### Where to install
@@ -26,13 +36,26 @@ Have an iCloud account. Open your console of choice and type `cn-calendar setup`
 You can create an app specific password by signing in to the [Apple ID account page](https://appleid.apple.com/account/manage). Then under "Security" > "App Specific Passwords" click "Create password..." and follow the steps.  
 To get the next three values required go to [https://icloud.com/calendar/](https://icloud.com/calendar/), create a clean calendar ["Edit" -> "+"] and select it. (It is important that this calendar is not used by / for anything but this package.) Open your [developer tools](https://www.stramaxon.com/2013/10/developer-tools-in-browser.html) and go to the network tab (you might have to refresh the page to have all info shown correctly). Locate the entry named "startup" and find the following 3 values:  
 1. the two digits after the "https://p" in the URL
-   ![Screenshot 1](screenshot1.jpg)
+   ![Screenshot 1](img/screenshot1.jpg)
 2. the DSid (located in the parameters of the request) and
-   ![Screenshot 2](screenshot2.jpg)
+   ![Screenshot 2](img/screenshot2.jpg)
 3. the pGuid (located in the response of the server)
-   ![Screenshot 3](screenshot3.jpg)
+   ![Screenshot 3](img/screenshot3.jpg)
 ### Starting
 To start, type `cn-calendar` or `cn-calendar run`.
-Use the flag `--once` to run the script once (and not perpetually, twice every day).
+Use the flag `--once` to run the script once (and not perpetually, twice every day). To see the log for the current process, type `cn-calendar log`. These are deleted everytime you start the package.
 ### Stopping
 To stop, type `cn-calendar stop`.
+
+## Contributing
+This package currently only works with Apple Calendar. However, not all people use Apple Calendar as their primary calendar. Help to enable them using this package with their favourite calendar app.  
+This is the current state:
+
+| Calendar | Supported | Notes |
+|---|:---:|---|
+| Apple Calendar | ✓ |  |
+| Google Calendar |  | Google API not suitable for small projects |
+| Microsoft Calendar / Outlook |  |  |
+| _others?_ |  |  |
+
+To understand the code and contribute, refer to [contributing.md](contributing.md)
--- cli.js
+++ cli.js
@@ -1,18 +1,22 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
 const fs = require('fs')
-const readlineSync = require('readline-sync');
 const readline = require('readline')
 const path = require("path");
+const forever = require('forever')
+const promptly = require('promptly')
 
 const args = [...process.argv]
 args.shift()
 args.shift()
 
 const credentials_path = path.join(process.mainModule.path, 'credentials.json')
+const interval_path = path.join(process.mainModule.path, 'lib', 'interval.js')
+const index_path = path.join(process.mainModule.path, 'lib', 'index.js')
 
 const checkFlags = (...flags) => {
     let options = [...args]
-    options.shift()
+    if (!options[0].startsWith('-'))
+        options.shift()
     let foundFlags = []
     options.forEach(arg => {
         if (flags.indexOf(arg) === -1) {
@@ -24,53 +28,123 @@ const checkFlags = (...flags) => {
     return foundFlags
 }
 
+forever.load({
+    root: path.join(process.mainModule.path, 'forever')
+})
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-process.stdin.setRawMode(false);
+
+/*function confirm(message, failMsg, def) {
+
+    return new Promise(r => {
+        let data = `${message}${message.endsWith(' ') ? '' : ' '}[${
+            (def === null || def === undefined) ? 'y/n' :
+                (def ? 'Y/n' : 'N/y')
+        }]: `;
+        rl.write(data)
+        readline.emitKeypressEvents(process.stdin);
+        process.stdin.setRawMode(true);
+        const kpHandler = (str, key) => {
+            process.stdin.removeAllListeners()
+            process.stdin.setRawMode(false);
+            process.stdin.resume()
+            rl.write('\n')
+            rl.resume()
+            switch (key.name) {
+                case 'y':
+                    return r(true)
+                case 'n':
+                    return r(false)
+                case 'return':
+                    return r(def)
+                default:
+                    process.stdin.on('keypress', kpHandler)
+                    rl.write(failMsg + (failMsg.endsWith(' ') ? '' : ' '))
+            }
+        }
+        process.stdin.on('keypress', kpHandler)
+    })
+}*/
+
+function confirm(message, failMsg, def) {
+    return new Promise(r => {
+        let data = `${message}${message.endsWith(' ') ? '' : ' '}[${
+            (def === null || def === undefined) ? 'y/n' :
+                (def ? 'Y/n' : 'N/y')
+        }]: `;
+        // rl.write(data)
+
+        const kpHandler = (s, key) => {
+            process.stdin.removeListener('keypress', kpHandler)
+            rl.resume()
+            switch (key.name) {
+                case 'y':
+                    return r(true)
+                case 'n':
+                    return r(false)
+                case 'return':
+                    return r(def)
+                default:
+                    rl.write(failMsg + (failMsg.endsWith(' ') ? '' : ' '))
+                    rl.resume()
+                    process.stdin.on('keypress', kpHandler)
+            }
+        }
+        process.stdin.on('keypress', kpHandler)
+    })
+}
+
+rl.question = (message, secret) => {
+    return new Promise(r => {
+        message = message + message.endsWith(' ') ? '' : ' ';
+        if (!secret)
+            rl.question(message, (answer) => {
+                r(answer);
+            })
+        else {
+            rl.write(message)
+            let answer = '';
+            const kpHandler = (s, key) => {
+                if (key.name === 'return') {
+                    process.stdin.removeListener('keypress', kpHandler)
+                    rl.resume()
+                    r(answer)
+                } else
+                    answer += s
+            }
+            process.stdin.on('keypress', kpHandler)
+        }
+    })
+}
+
+function isRunning() {
+    return new Promise(resolve => {
+        forever.list(false, (r, l) => {
+            resolve(!!l)
+        })
+    })
+}
 
 (async () => {
     switch (args[0]) {
         case 'setup': {
             const options = checkFlags('-y')
 
-            function confirm(message, def) {
-                return new Promise(r => {
-                    let data = `${message} [${(def === null || def === undefined) ? 'y/n' : (def ? 'Y/n' : 'N/y')}]: `;
-                    rl.write(data)
-                    readline.emitKeypressEvents(process.stdin);
-                    process.stdin.setRawMode(true);
-                    process.stdin.on('keypress', (str, key) => {
-                        process.stdin.setRawMode(false);
-                        rl.write('\n')
-                        switch (key.name) {
-                            case 'y':
-                                return r(true)
-                            case 'n':
-                                return r(false)
-                            case 'return':
-                                return r(def)
-                            default: {
-                                rl.write('Press [Y] to overwrite, or [N] to abort. ')
-                                process.stdin.setRawMode(true);
-                            }
-                        }
-                    });
-                })
-            }
-
             if (fs.existsSync(credentials_path) && !options.includes('-y')) {
-                if (await confirm('You have already run the setup. Running it again will overwrite the current setup.\nDo you want to continue?', false))
+                if (await confirm(
+                    'You have already run the setup. Running it again will overwrite the current setup.\nDo you want to continue?',
+                    'Press [Y] to overwrite, or [N] to abort.',
+                    false))
                     console.log('\nLeave any field blank to keep the previous value.')
                 else
                     process.exit(0)
             }
             // do the setup
-            let credentials = {
-                icloud: {},
-                campusnet: {},
-            }
+            let credentials = JSON.parse(fs.writeFileSync(credentials_path, {encoding: 'utf8'}))
+
             credentials.icloud.email = readlineSync.question('icloud email: ') || credentials.icloud.email
             credentials.icloud.password = readlineSync.question('icloud app-specific password: ', {
                 hideEchoBack: true
@@ -82,28 +156,60 @@ process.stdin.setRawMode(false);
             credentials.campusnet.password = readlineSync.question('campusnet password: ', {
                 hideEchoBack: true
             }) || credentials.campusnet.password
+
             fs.writeFileSync(credentials_path, JSON.stringify(credentials), {encoding: 'utf8'})
             break
         }
         case undefined:
         // alias 'run'
         case 'run': {
-            const options = checkFlags('-q', '--quiet', '--once')
+            const options = checkFlags('-q', '--quiet', '--once', '-y')
             // check if setup has been done
             if (!fs.existsSync(credentials_path)) {
                 console.error('You must run `cn-calendar setup` before usage.\n' +
                     'Please follow the instructions at https://github.com/drinking-code/cn-to-cal#setup')
                 process.exit(2)
             }
-            // start background process
+            if (await isRunning() && !options.includes('-y') && !options.includes('--once')) {
+                if (!await confirm(
+                    'The script is already running. Do you want to restart?',
+                    'Press [Y] to restart, or [N] to abort.',
+                    false))
+                    process.exit(0)
+            } else if (options.includes('--once')) {
+                require('child_process').fork(index_path)
+                    .on('exit', code => {
+                        process.exit(code)
+                    })
+            } else {
+                forever.cleanLogsSync()
+                // start background process
+                forever.startDaemon(interval_path);
+                if (!options.includes('-q') && !options.includes('--quiet')) {
+                    console.log('The script is now running.')
+                    console.log('Run `cn-calendar stop` to stop it.')
+                }
+            }
             break
         }
         case 'stop': {
-            const options = checkFlags()
+            checkFlags()
             // stop background process
+            let stopEmitter = forever.stopAll()
+            stopEmitter.on('error', err => {
+                console.error('The script is already stopped.')
+            })
+            stopEmitter.on('stopAll', res => {
+                console.log('Stopped the script successfully.')
+            })
+            break
+        }
+        case 'list': {
+            forever.list(false, console.log)
             break
         }
         default:
-        // error unknown command
+            // error unknown command
+            console.error(`Unknown command: '${args[0]}'`)
     }
 })()
